750daefa2d871c11e0cebd015e1c279a62ecceaf
Matyas Horkay <horkay.matyas@gmail.com>
2019-02-28 19:05:17 +0000

    queueueu

--- src/Processor.java
+++ src/Processor.java
@@ -1,15 +1,31 @@
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Processor {
     private Scanner input;
-    private Queue<ImageV> verticalImages;
-    private Queue<ImageH> horizontalImages;
+    private Set<Image> verticalImagesSet;
+    private Set<Image> horizontalImagesSet;
+    private Queue<Image> verticalQueue;
+    private Queue<Image> horizontalQueue;
 
-    public Processor(Queue<ImageV> verticalImages, Queue<ImageH> horizontalImages){
-        this.verticalImages = verticalImages;
-        this.horizontalImages = horizontalImages;
+    public Processor(Set<Image> verticalImages, Set<Image> horizontalImages){
+        this.verticalImagesSet = verticalImages;
+        this.horizontalImagesSet = horizontalImages;
+        this.verticalQueue = createQueue(verticalImagesSet);
+        this.horizontalQueue = createQueue(horizontalImagesSet);
     }
 
+    private Queue<Image> createQueue(Set<Image> set){
+        Queue<Image> result = new PriorityQueue<>(set.size(), idComparator);
+        result.addAll(set);
+        return result;
+    }
+
+    private static Comparator<Image> idComparator = new Comparator<Image>() {
+        @Override
+        public int compare(Image o1, Image o2) {
+            return (int) o1.getId() - o2.getId();
+        }
+    };
+    
 
 }
