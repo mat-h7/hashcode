4b5524382d09432dc02cb022fe82f15670d99157
Matyas Horkay <horkay.matyas@gmail.com>
2019-02-28 19:45:51 +0000

    y

--- src/OutputHandler.java
+++ src/OutputHandler.java
@@ -4,13 +4,22 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.sql.SQLOutput;
 import java.util.List;
+import java.util.Set;
 
 public class OutputHandler {
 
     private PrintStream printStream;
     private OutputStream outputStream;
+    private Set<Slide> verticalSlides;
+    private Set<Slide> horizontalSlides;
 
-    public OutputHandler(String path, List<Slide> slides) {
+    public OutputHandler(Set<Slide> verticalSlides, Set<Slide> horizontalSlides, PrintStream outputStream){
+        this.verticalSlides = verticalSlides;
+        this.horizontalSlides = horizontalSlides;
+        this.outputStream = outputStream;
+    }
+
+    /*public OutputHandler(String path, List<Slide> slides) {
         try {
             outputStream = new FileOutputStream(path);
             printStream = new PrintStream(outputStream);
@@ -21,7 +30,7 @@ public class OutputHandler {
         catch (NullPointerException e) {
             System.out.println("outputStream is null");
         }
-    }
+    }*/
 
 
 
