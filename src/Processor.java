f713290bfa25741e4e50eb528c6e1b8d191cb911
Matyas Horkay <horkay.matyas@gmail.com>
2019-02-28 19:23:31 +0000

    slides

--- src/Processor.java
+++ src/Processor.java
@@ -6,17 +6,33 @@ public class Processor {
     private Set<Image> horizontalImagesSet;
     private Queue<Image> verticalQueue;
     private Queue<Image> horizontalQueue;
+    private Set<Slide> slides = new HashSet<>();
 
     public Processor(Set<Image> verticalImages, Set<Image> horizontalImages){
         this.verticalImagesSet = verticalImages;
         this.horizontalImagesSet = horizontalImages;
         this.verticalQueue = createQueue(verticalImagesSet);
         this.horizontalQueue = createQueue(horizontalImagesSet);
+        createSlides();
+    }
+
+    public void createSlides(){
+        Queue<Image> queue = (ArrayDeque<Image>) verticalQueue;
+        while(!queue.isEmpty()){
+            Image first = ((ArrayDeque<Image>) queue).getFirst();
+            Image last = ((ArrayDeque<Image>) queue).getLast();
+            slides.add(new Slide(first, last));
+        }
     }
 
     private Queue<Image> createQueue(Set<Image> set){
-        Queue<Image> result = new PriorityQueue<>(set.size(), idComparator);
-        result.addAll(set);
+        Queue<Image> pQueue = new PriorityQueue<>(set.size(), idComparator);
+        pQueue.addAll(set);
+        Queue<Image> result = new ArrayDeque<>();
+        result.addAll(pQueue);
+        System.out.println(((ArrayDeque<Image>) result).getLast().getId());
+        System.out.println(((ArrayDeque<Image>) result).getFirst().getId());
+
         return result;
     }
 
@@ -27,5 +43,8 @@ public class Processor {
         }
     };
 
-
+    public Set<Slide> getSlides(){
+        return slides;
+    }
+    
 }
