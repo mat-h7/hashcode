48d67e6f878d08ab69242ce2ec16ac890682e05d
David Mark Valaczkai <DMV18@ic.ac.uk>
2019-02-28 18:38:21 +0000

    Slide class

---
+++ src/Slide.java
@@ -0,0 +1,43 @@
+public class Slide {
+
+    private enum Type {
+        Horizontal,
+        Vertical
+    }
+
+    private Image img1, img2;
+    private Type type;
+    private Slide next;
+    private int id;
+
+    public Slide(Image image) {
+        img1 = image;
+        type = Type.Horizontal;
+    }
+
+    public Slide(Image img1, Image img2) {
+        this.img1 = img1;
+        this.img2 = img2;
+        type = Type.Vertical;
+    }
+
+    public Image getImg1() {
+        return img1;
+    }
+
+    public Image getImg2() {
+        return img2;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Slide getNext() {
+        return next;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
